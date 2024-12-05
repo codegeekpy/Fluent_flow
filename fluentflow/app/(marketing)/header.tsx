2630b1da335c15e0403d319645393c9743712237
codegeekpy <mechastro768@gmail.com>
2024-12-05 22:17:57 +0530

    done with sigin

--- fluentflow/app/(marketing)/header.tsx
+++ fluentflow/app/(marketing)/header.tsx
@@ -1,33 +1,46 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  ClerkLoading,
+  ClerkLoaded,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import {SignInButton} from "@clerk/nextjs";
 
-import Image from "next/image"; // Ensure this is correctly imported
-import { ClerkLoading,
-    ClerkLoaded,
-    SignedIn,
-    SignedOut,
-    SignInButton,
-    UserButton
- } from "@clerk/nextjs";
-import {Loader} from "lucide-react";
-import { Sign } from "crypto";
+
+const SignInButtonProps = { mode: 'modal', afterSignInUrl: '/learn', afterSignUpUrl: '/learn',};
+
+
+
 export const Header = () => {
   return (
-    <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+    <header className="h-20 w-full border-b-2 border-slate-400 px-4">
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full bg-slate-200">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
           <Image alt="logo" src="/logo.svg" height={40} width={40} />
-          <h1 className="text-2xl font-extrabold text-orange-600 tracking-wide">FluentFlow</h1>
+          <h1 className="text-2xl font-extrabold text-orange-600 tracking-wide">
+            FluentFlow
+          </h1>
         </div>
         <ClerkLoading>
- <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
         </ClerkLoading>
         <ClerkLoaded>
-            <SignedIn>
-              <UserButton></UserButton>
-            </SignedIn>
-            <SignedOut>
-                <SignInButton>
-                </SignInButton>
-            </SignedOut>
+          <SignedIn>
+            <UserButton 
+            afterSignOutUrl="/"
+            />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton>
+              <Button size="lg" variant="ghost">
+               LOGIN
+              </Button>
+            </SignInButton>
+          </SignedOut>
         </ClerkLoaded>
       </div>
     </header>
